4b556ad208a1ab101c0845da3ac82b71c4193926
ansndsc <ansndsc@gmail.com>
2021-12-16 10:03:43 -0300

    Adiciona controller update e remove

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -22,11 +22,46 @@ const signUp = async (req, res) => {
 };
 
 const update = async (req, res) => {
+    
+    try {
+        const foundEmail = await req.params.email;
+        if (foundEmail) {
+            /*
+            //                  valor que quero mudar  ou  valor que já existe
+            musicaEncontrada.artista = req.body.artista || musicaEncontrada.artista
+            musicaEncontrada.album = req.body.album || musicaEncontrada.album
+            musicaEncontrada.ano = req.body.ano || musicaEncontrada.ano
+            musicaEncontrada.titulo = req.body.titulo || musicaEncontrada.titulo
 
+            const musicaSalva = await musicaEncontrada.save()
+            */
+            res.status(200).json(foundEmail);
+        }
+
+        res.status(400).json({
+            mensagem: "Descupa, mas não conseguimos encontrar essa musica"
+        })
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
+    
 };
 
 const remove = async (req, res) => {
+    try {
+        const foundId = await UserSchema.findById(req.params.id)
 
+       await foundId.delete()
+
+       res.status(204)
+
+    } catch (error) {
+        res.status(400).json({
+            mensagem: error.message
+        })
+    }
 };
 
 module.exports = {
