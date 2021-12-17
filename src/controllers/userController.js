43cf02964e8d3283ead702b0e679a078aa726eaf
ansndsc <ansndsc@gmail.com>
2021-12-17 14:30:58 -0300

    Atualiza controller update e delete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,10 @@
+const mongoose = require('mongoose');
 const UserSchema = require('../models/userSchema');
 
 const getUsers = async (req, res) => {
     try {
         const users = await UserSchema.find();
+        
         res.status(200).json(users);
 
     } catch (error) {
@@ -12,9 +14,20 @@ const getUsers = async (req, res) => {
 
 const signUp = async (req, res) => {
     try {
-        const user = await UserSchema.create(req.body);
+        
+        const user = new UserSchema({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            availableDate: req.body.availableDate,
+            availableTime:req.body.availableTime,
+            socialMedia:req.body.socialMedia,
+            _id: new mongoose.Types.ObjectId()
+        });
 
-        return res.status(201).send({ user });
+        const savedUser = await user.save();
+
+        res.status(201).json({ User: savedUser });
 
     } catch (error) {
         res.status(500).send({ message: error.message });
@@ -24,43 +37,49 @@ const signUp = async (req, res) => {
 const update = async (req, res) => {
     
     try {
-        const foundEmail = await req.params.email;
-        if (foundEmail) {
-            /*
-            //                  valor que quero mudar  ou  valor que já existe
-            musicaEncontrada.artista = req.body.artista || musicaEncontrada.artista
-            musicaEncontrada.album = req.body.album || musicaEncontrada.album
-            musicaEncontrada.ano = req.body.ano || musicaEncontrada.ano
-            musicaEncontrada.titulo = req.body.titulo || musicaEncontrada.titulo
 
-            const musicaSalva = await musicaEncontrada.save()
+        const foundId = await UserSchema.findById(req.params.id);
+        //const bodyReq = req.body;
+        if (foundId) {
+            
+            foundId.name = req.body.name || foundId.name
+            foundId.email = req.body.email || foundId.email
+            foundId.password = req.body.password || foundId.password
+            foundId.avalilableDate = req.body.avalilableDate || foundId.avalilableDate
+            foundId.availableTime = req.body.availableTime || foundId.availableTime
+            foundId.socialMedia = req.body.socialMedia || foundId.socialMedia
+            /*
+            Object.keys(foundId).forEach((informacao)=>{
+                if(bodyReq[informacao] == undefined){
+                    foundId[informacao] = foundId[informacao]
+                } else {
+                    foundId[informacao] = bodyReq[informacao]
+                }
+            });
             */
-            res.status(200).json(foundEmail);
+            const savedUser = await foundId.save();
+            
+            res.status(200).json(savedUser);
         }
 
-        res.status(400).json({
-            mensagem: "Descupa, mas não conseguimos encontrar essa musica"
-        })
+        //res.status(404).json({ message: `ID não encontrado.` });
+
     } catch (error) {
-        res.status(400).json({
-            mensagem: error.message
-        })
+        res.status(400).json({ message: error.message });
     }
     
 };
 
 const remove = async (req, res) => {
     try {
-        const foundId = await UserSchema.findById(req.params.id)
+        const foundId = await UserSchema.findById(req.params.id);
 
-       await foundId.delete()
+        await foundId.delete();
 
-       res.status(204)
+        res.status(200).json({ message: `User deletado com sucesso.`});
 
     } catch (error) {
-        res.status(400).json({
-            mensagem: error.message
-        })
+        res.status(400).json({ message: error.message });
     }
 };
 
